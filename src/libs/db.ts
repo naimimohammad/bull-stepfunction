5d5d68a67db6e98c844137ba7732297700fc3720
Mohammad Naimi <naimi.92@gmail.com>
2022-06-26 21:47:12 +0430

    add redis lib

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -2,44 +2,59 @@ import * as redis from 'redis'
 export class redisCon {
     /*
     */
-    client
+    client;
     constructor(redisUri:string){
         this.client = redis.createClient({url: redisUri})
+        
             this.connect()
     }
     
     async connect() {
          await this.client.connect()
     }
-    public async flushAll() {
-        this.client.FLUSHALL()
-        .then(r=>{
-            this.client.set('failedNum',0)
-            this.client.set('successNum',0)
-            console.log('redis has been flushed')
-        })
-        .catch(e=>{
-            console.log('redis flushed has problem')
-        })
-    }
+
 
     
-    public async addFailed () {
-        this.client.incr('failedNum')
+    public async Set (type:string,Id:string,value:any) {
+       return await this.client.set(`${type}:${Id}`,(typeof value === 'object')?JSON.stringify(value):value)
 
     }
-    public async getFailed () {
-        return await this.client.get('failedNum')
+    public async Get (type:string,Id:string) {
+        let result:any = await this.client.get(`${type}:${Id}`)
+        if (type == 'IdResult'){
+            try {
+                return JSON.parse(result)
+            } catch (error) {
+                return result
+            }
+        }
     }
-    public async addSuccess () {
-        this.client.incr('successNum')
+    public async pushResult (Id:string,index:number,value:any) {
+        Promise.all([this.client.lSet(`Result:${Id}`,index,(typeof value === 'object')?JSON.stringify(value):value),
+            this.client.incr(`Done:${Id}`)]
+        )
         
     }
-    public async getSuccess () {
-       
-        return await this.client.get('successNum')
+
+    public async getResult (Id:string) {
+        let result:any = await this.client.get(`Result:${Id}`)
+        result=result.map((item:any)=>
+            this.checkObj(item)
+        )
+        return result
     }
-    public async getTotal () {
-        return await this.client.get('totalNum')
+
+
+    public async getDone (Id:string) {
+        return await this.client.get(`Done:${Id}`)
+    }
+    checkObj(value:any){
+        try {
+            return JSON.parse(value)
+        } catch (error) {
+            return value
+        }
     }
 }
+
+
